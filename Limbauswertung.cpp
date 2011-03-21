19cf1ed3c23c46c1654e0b3cc0509f29842ef08d
Stefan Bender <stefan.bender@kit.edu>
2011-03-21 15:32:36 +0100

    iterators for loops
    Since we are dealing with vectors (already done by M.L.), we use
    iterators for the loops instead.
    
    There is no need to resize Rohdaten since it will be deleted when we
    leave Limb_Auswertung() anyway.

--- Limbauswertung.cpp
+++ Limbauswertung.cpp
@@ -19,6 +19,7 @@
  *************************************************************************/
 
 #include <vector>
+#include <iterator>
 #include "Orbitliste.h"
 #include "Sonnenspektrum.h"
 #include "Speziesfenster.h"
@@ -111,22 +112,24 @@ int Limb_Auswertung(Orbitliste Orbitlist,
 	// Jedes Element von Rohdaten wird nun in seperaten Schritten mit den
 	// Speziesdaten aufgefüllt und die Bestimmung der Zeilendichte wird
 	// durchgeführt
-	for (i = 0; i < Rohdaten.size(); i++) { //Schleife über alle Rohdaten
-		Messung_Limb Messung = Rohdaten[i];
+	vector<Messung_Limb>::iterator mlit;
+	vector<Speziesfenster>::iterator sfit;
+	//Schleife über alle Rohdaten
+	for (mlit = Rohdaten.begin(); mlit != Rohdaten.end(); ++mlit) {
 		//Übergabe der Rohdaten, sodass diese nicht verändert werden
 		// wegen Parallelisierung
 		// sin und cos sind langsame Funktionen...
 		// werden aber hierbei auch nicht oft eingesetzt
-		Messung.Deklinationswinkel_bestimmen(); //Sonnenlattitude
-		Messung.Sonnen_Longitude_bestimmen();
-		Messung.Intensitaeten_normieren(Solspec.m_Int_interpoliert);
+		(*mlit).Deklinationswinkel_bestimmen(); //Sonnenlattitude
+		(*mlit).Sonnen_Longitude_bestimmen();
+		(*mlit).Intensitaeten_normieren(Solspec.m_Int_interpoliert);
 		// m_Intensitaeten enthält nun nichtmehr I sondern I/(piF)
 		// Das könnte man auch nur für die Par Fenster durchführen
 		//cout<<Messung.m_Intensitaeten_durch_piF[0]<<"\n";
-		for (j = 0; j < Spezies_Fenster.size(); j++) {
-			//Schleife über alle Spezies wie z.b. Mg oder Mg+
-			for (k = 0; k < Spezies_Fenster[j].m_Wellenlaengen.size(); k++) {
-				//Schleife über alle Linien dieser Spezies
+		//Schleife über alle Spezies wie z.b. Mg oder Mg+
+		for (sfit = Spezies_Fenster.begin(); sfit != Spezies_Fenster.end(); ++sfit) {
+			//Schleife über alle Linien dieser Spezies
+			for (k = 0; k < (*sfit).m_Wellenlaengen.size(); k++) {
 				// Aus SZA_TP und SAA_TP lässt sich die Polararisation in den
 				// Liniendaten des Speziesfensters ermitteln, und damit die
 				// Emissivität berechnen
@@ -134,14 +137,14 @@ int Limb_Auswertung(Orbitliste Orbitlist,
 				//der Streuwinkel muss woanders berechnet werden
 				// Die Phasenfunktion, steckt so nun in den Slant Coloumns drin
 				//cout<<Spezfenst.m_Liniendaten[k].m_theta<<"\n";
-				Spezies_Fenster[j].m_Liniendaten[k].Emissivitaet_ermitteln();
+				(*sfit).m_Liniendaten[k].Emissivitaet_ermitteln();
 				//cerr<<Spezies_Fenster[j].m_Basisfenster_rechts_WLmin[k]<<"\n";
 				//cerr<<"j:"<<j<<"\t"<<"k:"<<k<<"\t"
 				//  <<Spezies_Fenster[j].m_Wellenlaengen[k]<<"\t"
 				//  <<Spezies_Fenster[j].m_Liniendaten[k].m_E1<<"\t"
 				//  <<Spezies_Fenster[j].m_Liniendaten[k].m_Gamma<<"\n";
-				Messung.Intensitaeten_durch_piF_Gamma_berechnen(Spezies_Fenster[j], k);
-				Messung.Intensitaeten_durch_piF_Gamma_mal_Gitterabstand_berechnen(Spezies_Fenster[j], k);
+				(*mlit).Intensitaeten_durch_piF_Gamma_berechnen((*sfit), k);
+				(*mlit).Intensitaeten_durch_piF_Gamma_mal_Gitterabstand_berechnen((*sfit), k);
 				// In der Formel ist piF in W/(m^2*Wellenlänge) verlangt..
 				// also muss noch mit der Kanalbreite multipliziert werden
 				//cout<<Messung.m_Intensitaeten_durch_piF_Gamma[1]<<"\n";
@@ -159,7 +162,7 @@ int Limb_Auswertung(Orbitliste Orbitlist,
 //                else
 //                {
 				// Hmm hier gibts noch Diskussionsbedarf
-				Messung.Zeilendichte_Bestimmen(Spezies_Fenster[j], k,
+				(*mlit).Zeilendichte_Bestimmen((*sfit), k,
 						Arbeitsverzeichnis, mache_Fit_Plots);
 //                }
 
@@ -173,14 +176,14 @@ int Limb_Auswertung(Orbitliste Orbitlist,
 
 				// Ergebnis zusammenfassen
 				Ausgewertete_Messung_Limb Ergebnis
-					= Messung.Ergebnis_Zusammenfassen();
+					= (*mlit).Ergebnis_Zusammenfassen();
 				// Die braucht man später für die Luftmassenmatrix
 				Ergebnis.m_Wellenlaenge
-					= Spezies_Fenster[j].m_Wellenlaengen[k];
+					= (*sfit).m_Wellenlaengen[k];
 				//Ergebnis.Ausgabe_auf_Bildschirm();
 				// Zusammenfassung der Zwischenresultate dem Vektor für die
 				// jeweilige Spezies zuordnen
-				if (Spezies_Fenster[j].m_Spezies_Name == "MgI") {
+				if ((*sfit).m_Spezies_Name == "MgI") {
 					//TODO negative Werte zulassen
 					if (Ergebnis.m_Zeilendichte > 0) {
 						Ausgewertete_Limbmessung_MgI.push_back(Ergebnis);
@@ -193,23 +196,20 @@ int Limb_Auswertung(Orbitliste Orbitlist,
 					//cout<<Ausgewertete_Limbmessung_MgI[0].m_Zeilendichte<<"\n";
 					//-das geht
 				}
-				if (Spezies_Fenster[j].m_Spezies_Name == "MgII") {
+				if ((*sfit).m_Spezies_Name == "MgII") {
 					Ausgewertete_Limbmessung_MgII.push_back(Ergebnis);
 				}
-				if (Spezies_Fenster[j].m_Spezies_Name == "unknown") {
+				if ((*sfit).m_Spezies_Name == "unknown") {
 					Ausgewertete_Limbmessung_unknown.push_back(Ergebnis);
 				}
-				if (Spezies_Fenster[j].m_Spezies_Name == "FeI") {
+				if ((*sfit).m_Spezies_Name == "FeI") {
 					Ausgewertete_Limbmessung_FeI.push_back(Ergebnis);
 				}
 
 			}//ende k Linie
 		}//ende j Spezies_Fenster
 	}//ende i Rohdaten
-	// Die Rohdaten werden nun nichtmehr gebraucht, sodass alle dynamisch
-	// allokierten Felder gelöscht werden können
 
-	Rohdaten.resize(0);
 	return 0; // keine Probleme
 }
 ////////////////////////////////////////////////////////////////////////////////
