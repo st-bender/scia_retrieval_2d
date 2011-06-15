50765704ba033baaf8f4c708fcce97bc4227ca18
Stefan Bender <stefan.bender@kit.edu>
2011-06-15 13:11:13 +0200

    filenames: rewrite orbit and date finding functions
    Re-writes the functions that return the orbit number and the date from
    the orbit-list and limb filenames, respectively. Uses sb_basename() and
    std::string methods to find them.

--- Dateinamensteile_Bestimmen.cpp
+++ Dateinamensteile_Bestimmen.cpp
@@ -25,11 +25,13 @@ std::string sb_basename(std::string filename)
 ////////////////////////////////////////////////////
 string xxxxx_Bestimmen(string Orbitlistenpfad)
 {
-	int lang = Orbitlistenpfad.size();
-	//string xxxxx=Orbitlistenpfad.substr(lang-20,5);  old
-	string xxxxx = Orbitlistenpfad.substr(lang - 15, 5);
-	// cout<<xxxxx<<"\n";
-	return xxxxx;
+	std::string bname = sb_basename(Orbitlistenpfad);
+	std::string::size_type pos = bname.find_first_of("0123456789");
+
+	if (pos != std::string::npos)
+		return bname.substr(pos, 5);
+	else
+		return "xxxxx";
 }
 ////////////////////////////////////////////////////
 // ENDE xxxxx_Bestimmen
@@ -40,15 +42,17 @@ string xxxxx_Bestimmen(string Orbitlistenpfad)
 ////////////////////////////////////////////////////
 string yyyymmdd_hhmm_Bestimmen(string Name_erste_Limbdatei)
 {
-	string yyyymmdd_hhmm;
 	// Die Datei ist im MPL_binary Format
 	// z.b. SCIA_limb_20040111_084344_1_0_09752.dat.l_mpl_binary
-	int lang = Name_erste_Limbdatei.size();
-	yyyymmdd_hhmm = Name_erste_Limbdatei.substr(lang - 42, 13);
+	std::string bname = sb_basename(Name_erste_Limbdatei);
+	std::string::size_type pos = bname.find_first_of("0123456789");
+
 	//TODO Das eventuell ganz umbennnen...
 	// suchen eigentlich nur Datum...Uhrzeit Wuascht
-	yyyymmdd_hhmm = Name_erste_Limbdatei.substr(lang - 42, 8);
-	return yyyymmdd_hhmm;
+	if (pos != std::string::npos)
+		return bname.substr(pos, 8);
+	else
+		return "yyyymmdd";
 }
 ////////////////////////////////////////////////////
 // ENDE yyyymmdd_hhmm
