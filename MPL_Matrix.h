9aa34dd5e576bf0a5143ce619c1e4844fedc60a3
Stefan Bender <stefan.bender@kit.edu>
2013-11-05 15:48:37 +0100

    MPL_Matrix: fix dgemm_ prototype
    Fixes the interface prototype of the blas dgemm_() function.

--- MPL_Matrix.h
+++ MPL_Matrix.h
@@ -52,7 +52,7 @@
 
 extern "C" {
 	void dgemm_(char *TRANSA, char *TRANSB, int *M, int *N, int *K,
-				double *ALPHA, double *A, int *LDA, double *B, int *LDB,
+				double *ALPHA, const double *A, int *LDA, const double *B, int *LDB,
 				double *BETA, double *C, int *LDC);
 }
 //////////////////////////////////////////////////////
