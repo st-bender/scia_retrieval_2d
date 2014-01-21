9a3e6f114821090bc55664fbb0643de81f59818a
Stefan Bender <stefan.bender@kit.edu>
2014-01-21 11:03:27 +0100

    IO: update wavelength range for limb averaging
    Extends the wavelength range and uses the median instead of the mean
    for calculating the background signal. This should give more usable
    results when the background signal seems to vary within an orbit.
    Note that it is still commented, though.

--- Datei_IO.cpp
+++ Datei_IO.cpp
@@ -81,9 +81,9 @@ vector<Messung_Limb> make_messung_limb_vector(string Dateiname,
 	/*
 	// normal average or median for the dark signal correction
 	dark_sig = average_over_wl_range(Limbdaten[no_of_alt - 1].m_radiance,
-			Wellenlaengen, no_of_pix, 278.0, 282.0, false);
+			Wellenlaengen, no_of_pix, 238.0, 282.0, true);
 	dark_err = average_over_wl_range(Limbdaten[no_of_alt - 1].m_error,
-			Wellenlaengen, no_of_pix, 278.0, 282.0, false);
+			Wellenlaengen, no_of_pix, 238.0, 282.0, true);
 	*/
 
 	/*
