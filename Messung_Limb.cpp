7731e0b0858585163c57925adbf1522f38dcbc38
Stefan Bender <stefan.bender@kit.edu>
2015-02-04 15:03:46 +0100

    limb analysis: Initialise the vectors for Rayleigh
    We can initialise the vectors for the Rayleigh cross section directly
    without using a usual C-array first. Reduces code size.

--- Messung_Limb.cpp
+++ Messung_Limb.cpp
@@ -263,18 +263,15 @@ void Messung_Limb::Zeilendichte_Bestimmen(Speziesfenster &Spezfenst, int Index,
 double sigma_rayleigh(double wl)
 {
 	/* ref.: planet. space sci., vol. 32, no. 6, pp 785-790, 1984 */
-	double wls_Fk[35] = { 200, 205, 210, 215, 220, 225, 230, 240, 250, 260,
+	std::vector<double> wl_Fk{ 200, 205, 210, 215, 220, 225, 230, 240, 250, 260,
 		270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400,
 		450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000 };
-	double Fks[35] = { 1.080, 1.077, 1.074, 1.072, 1.070, 1.068, 1.066, 1.064,
+	std::vector<double> F_ks{ 1.080, 1.077, 1.074, 1.072, 1.070, 1.068, 1.066, 1.064,
 		1.062, 1.060, 1.059, 1.057, 1.056, 1.055, 1.055, 1.054, 1.053, 1.053, 1.052,
 		1.052, 1.052, 1.051, 1.051, 1.051, 1.050, 1.049, 1.049, 1.048, 1.048,
 		1.048, 1.048, 1.047, 1.047, 1.047, 1.047, 1.047 };
 	double F_k, n = n_air(wl);
 	double nm1_div_NA = (n - 1.) / 2.687e-2;
-
-	std::vector<double> wl_Fk(wls_Fk, wls_Fk + 35);
-	std::vector<double> F_ks(Fks, Fks + 35);
 
 	F_k = interpolate(wl_Fk, F_ks, wl);
 
