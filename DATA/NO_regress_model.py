ddacd631a4f0f21444070f38e9ad91e6eb3051e9
Stefan Bender <stefan.bender@kit.edu>
2017-04-19 11:11:51 +0200

    NO regress model: PEP8 and clean-up
    Beautifies the code a bit towatds PEP8 and cleans it up little,
    i.e. remove some old commented stuff.
    
    Uses numpy.searchsorted() instead of bisect.bisect_left() to avoid the
    bisect dependence.

--- DATA/NO_regress_model.py
+++ DATA/NO_regress_model.py
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/env python
+#!/usr/bin/env python
 # vim: set fileencoding=utf-8
 
 from __future__ import absolute_import, division, print_function
@@ -9,7 +9,6 @@ import numpy as np
 import datetime as dt
 import dateutil.parser as dp
 import scipy.interpolate as sip
-import bisect
 import json
 try:
 	from StringIO import StringIO
@@ -22,20 +21,20 @@ mktime = lambda t: dt.datetime.strptime(bytes(t).decode(), "%H:%M")
 def get_solar_data(fname, scale=1.0):
 	sol_name = os.path.basename(fname).split('_')[1]
 	sol_data = np.genfromtxt(fname, dtype=None,
-			converters = {0: mkdate, 1: mktime})
+			converters={0: mkdate, 1: mktime})
 	sol_dates = sol_data['f0']
-	sol_values = sol_data['f2']*scale
+	sol_values = sol_data['f2'] * scale
 	return sol_name, sol_dates, sol_values
 
 def time_fun_vec(t, N=[1.0, 2.0], solvalues=[], offset=True, linear=False):
-	w = 2.*np.pi/365.25
+	wt = 2. * np.pi / 365.25 * t
 	try:
 		sval = [s[t] for s in solvalues]
 	except:
 		sval = [np.median(np.asarray(s.values())) for s in solvalues]
 	offs = [1.0] if offset else []
 	lin = [t] if linear else []
-	ret = offs + lin + [f(n*w*t) for f in [np.cos, np.sin] for n in N] + sval
+	ret = offs + lin + [f(n * wt) for f in [np.cos, np.sin] for n in N] + sval
 	return ret
 
 def time_fun_sol(tday, t0, solnames, solscales, sol_lags, solsubmin=1):
@@ -48,20 +47,20 @@ def time_fun_sol(tday, t0, solnames, solscales, sol_lags, solsubmin=1):
 			else:
 				dds = [days, ds][solsubmin - 2]
 				sidx = [list(sdates).index(t0 + dt.timedelta(x))
-						for x in [(d-t0).days for d in sdates if (d-t0).days in dds]]
+						for x in [(d - t0).days for d in sdates if (d - t0).days in dds]]
 				svalues -= svalues[sidx].min()
 		sdicts.append(dict(zip([(t - t0).days + sollag for t in sdates], svalues)))
 	return time_fun_vec(tday, solvalues=sdicts)
 
 def coeffs_alt_lat(coeffs, fields, alt=100.0, lat=67.5):
 	lats = np.unique(coeffs['lats'])
-	lats_right = lats + np.mean(0.5*np.diff(lats))
-	j = bisect.bisect_left(lats_right, lat)
+	lats_right = lats + np.mean(0.5 * np.diff(lats))
+	j = np.searchsorted(lats_right, lat)
 	if j >= lats.shape[0]:
 		# decrease index if it is too large
 		j = lats.shape[0] - 1
 	l1 = lats[j]
-	cfl1 = coeffs[coeffs['lats']==l1]
+	cfl1 = coeffs[coeffs['lats'] == l1]
 	alts = cfl1['alts']
 	return (alts, cfl1[fields].copy().view('<f8').reshape((-1, len(fields))))
 
@@ -85,8 +84,6 @@ def main():
 		alt = 100.0
 		lat = 67.5
 
-	#print >> sys.stderr, alt, lat
-
 	#basepath = os.path.dirname(sys.argv[0])
 	#if basepath == '':
 	#	basepath = '.'
@@ -101,19 +98,15 @@ def main():
 
 	fields = ["offset", "cos1", "sin1", "cos2", "sin2"] + solnames
 	data = np.genfromtxt(coeff_fname, dtype=None, names=True)
-	coeffs = data[data['name']==data_name]
-
-	#print >> sys.stderr, date, coeff_fname, t_day
-	#print >> sys.stderr, sol_files, solnames, sol_data
+	coeffs = data[data['name'] == data_name]
 
 	tfv = np.asarray(time_fun_sol(t_day, model_t0, sol_data, solscales,
 			sol_lags, solsubmin))
 	alts, cvs = coeffs_alt_lat(coeffs, fields, lat=lat)
 	mod_val = np.dot(cvs, tfv)
 	mod_val_f = sip.InterpolatedUnivariateSpline(alts, mod_val, k=1)
-	#mod_val_f = sip.UnivariateSpline(alts, mod_val, k=1)
-	#mod_val_f = sip.interp1d(alts, mod_val, bounds_error=False, fill_value=0)
 	print(' '.join(map(str, mod_val_f(np.atleast_1d(alt)))))
+
 
 if __name__ == "__main__":
 	main()
